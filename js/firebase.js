49156e83bbb2eeab82cfde23600a61c25bc74312
drobbe <edgard1394@hotmail.com>
2018-03-21 12:07:08 -0430

    Prototype to Upload img to firebase

--- js/firebase.js
+++ js/firebase.js
@@ -10,6 +10,12 @@ var config = {
 firebase.initializeApp(config);
 // Get a reference to the database service
 var database = firebase.database();
+// Get a reference to the storage service, which is used to create references in your storage bucket
+var storage = firebase.storage();
+// Create a root reference
+var storageRef = firebase.storage().ref();
+var file = null // use the Blob or File API
+var ref = null;
 writeUserData(2, "pedro2", "edgar2d@hoami.com", "cu2lo.png");
 
 function writeUserData(userId, name, email, imageUrl) {
@@ -23,4 +29,39 @@ firebase.database().ref('/users/').once('value').then(function(snapshot) {
     console.log(snapshot.val());
     // ...
 });
-console.log(username);
+$(document).ready(function() {
+    // Detect file element
+    $("#fileElem").change(function() {
+        // will log a FileList object, view gifs below
+        file = (this.files[0]);
+        fileName = (this.files[0].name);
+        //name file without extension
+        //fileName = (this.files[0].name.split('.')[0]);
+        // Create a storage reference from our storage service
+        ref = storageRef.child('img/' + fileName);
+    });
+});
+
+function uploadFile() {
+    var uploadTask = ref.put(file);
+    uploadTask.on('state_changed', function(snapshot){
+    }, function(error) {
+
+        // A full list of error codes is available at
+        // https://firebase.google.com/docs/storage/web/handle-errors
+        switch (error.code) {
+            case 'storage/unauthorized':
+                alert("User doesn't have permission to access the object");
+                break;
+            case 'storage/canceled':
+                alert("User canceled the upload");
+                break;
+            case 'storage/unknown':
+                alert("Unknown error occurred, inspect error.serverResponse");
+                break;
+        }
+    }, function() {
+        var downloadURL = uploadTask.snapshot.downloadURL;
+        console.log(downloadURL);
+    });
+}
